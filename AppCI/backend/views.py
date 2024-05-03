22a099c5576e6653787a9090f2e6169ffd862209
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-03 07:29:29 -0300

    feat: add template for testing app configuration

--- AppCI/backend/views.py
+++ AppCI/backend/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+""" View to load hello rhoni testing template """
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HelloRhoniView(TemplateView):
+    """Loads hello rhoni template
+    """
+
+    def get_template_names(self):
+        template_name = 'hello.html'
+        return template_name
