6eafd8f9ff98daf8298fbd30af967ad344bccf80
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-03 19:13:36 -0300

    Create the router folder for vueRouter for a better scaffolding

--- AppCI/frontend/src/main.js
+++ AppCI/frontend/src/main.js
@@ -1,15 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import HomeView from './views/HomeView.vue'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', name:'Home', component: HomeView },
-  ],
-})
+import router from '@/router'
 
 const app = createApp(App)
 app.use(router)
