8516316598217b5b9564e65d17f7f4e95f083c35
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-04 11:50:58 -0300

    refactor(frontend): change hello message

--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-      <h1 class="text-3xl font-bold underline">Hello rhonies!</h1>
+      <h1 class="text-3xl font-bold underline">Hello rhonies! this is the home in Vue </h1>
 </template>
 
 <style scoped></style>
