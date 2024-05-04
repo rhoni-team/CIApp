47268d72dd0b47b260e8a187407c71ebabadbd3d
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-04 16:11:38 -0300

    feat: add middleware whitenose configuration

--- AppCI/AppCI/settings.py
+++ AppCI/AppCI/settings.py
@@ -65,6 +65,8 @@ INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
