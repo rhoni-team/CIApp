33c51ad4172f72c40af81e90d34ccb5d9bfadda1
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-05 01:50:28 -0300

    refactor: Fixes by ESLint

--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -2,7 +2,9 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello rhonies! this is the home in Vue</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello rhonies! this is the home in Vue
+  </h1>
 </template>
 
 <style scoped></style>
