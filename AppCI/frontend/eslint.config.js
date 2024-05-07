c3c9906f599962924a2b8b91334ceddc9677937e
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-06 23:10:42 -0300

    chore: Add some Eslint dependencies

--- AppCI/frontend/eslint.config.js
+++ AppCI/frontend/eslint.config.js
@@ -1,12 +1,10 @@
-import pluginVue from 'eslint-plugin-vue'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
+
 export default [
-  // add more generic rulesets here, such as:
-  // js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
-  }
-]
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+];
