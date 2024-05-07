acd0fdf89556a5bfc8f7839a914df060c36ae202
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-07 10:16:07 -0300

    refactor: run lint

--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -3,10 +3,12 @@ import monet from "@/assets/img/MonetImpressionSunrise.jpg";
 </script>
 
 <template>
-      <h1 class="text-3xl font-bold underline">Hello rhonies! this is the home in Vue </h1>
-      <div>
-            <img :src="monet" />
-      </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello rhonies! this is the home in Vue
+  </h1>
+  <div>
+    <img :src="monet">
+  </div>
 </template>
 
 <style scoped></style>
