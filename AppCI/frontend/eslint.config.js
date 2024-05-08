f438efd083d38284b4a9a604226b79168c83998e
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-08 20:18:02 -0300

    chore: Exclude 'dist' folder from the linter targets

--- AppCI/frontend/eslint.config.js
+++ AppCI/frontend/eslint.config.js
@@ -3,7 +3,17 @@ import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/recommended"],
+  {
+    languageOptions: { 
+      globals: { 
+        ...globals.browser, 
+        ...globals.node 
+      } 
+    },
+  },
+  {
+    ignores: ["**/node_modules/**", "**/dist/**"], 
+  },
 ];
