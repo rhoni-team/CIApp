b11a037b82557a55e885564133573e67fec60a01
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-08 22:52:56 -0300

    refactor: Alter code to making the eslint test on PR fails

--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -3,8 +3,7 @@ import monet from "@/assets/img/MonetImpressionSunrise.jpg";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello rhonies! this is the home in Vue
+  <h1 class="text-3xl font-bold underline">    Hello rhonies! this is the home in Vue
   </h1>
   <div>
     <img :src="monet">
