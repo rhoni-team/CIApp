aa3287a277200bca5fe671bb37d2adb77e32c955
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-22 09:49:35 -0300

    feat: add cleaning type model

--- AppCI/backend/models.py
+++ AppCI/backend/models.py
@@ -5,5 +5,10 @@ from django.db import models
 class IsolationType(models.Model):
     """Model for 'Tipo de aislamiento'"""
     name = models.CharField(max_length=100, null=False, blank=False)
-    name = models.CharField(max_length=100, null=True, blank=True)
+    label = models.CharField(max_length=100, null=True, blank=True)
 
+
+class CleaningType(models.Model):
+    """Model for 'Tipo de limpieza'"""
+    name = models.CharField(max_length=100, null=False, blank=False)
+    label = models.CharField(max_length=100, null=True, blank=True)
