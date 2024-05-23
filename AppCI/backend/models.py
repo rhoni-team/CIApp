f914fa4544d124484a58845e7683869869683783
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-23 09:24:16 -0300

    feat: add disease model

--- AppCI/backend/models.py
+++ AppCI/backend/models.py
@@ -13,6 +13,9 @@ class CleaningType(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     label = models.CharField(max_length=100, null=True, blank=True)
 
+    def __str__(self):
+        return self.label
+
 
 class UnitsOfTime(models.Model):
     """Model with units of time"""
@@ -25,7 +28,7 @@ class IsolationTime(models.Model):
     name = models.CharField(max_length=300, blank=True, null=True)
     label = models.CharField(max_length=300, null=True, blank=True)
     isolation_time = models.IntegerField(blank=True, null=True)
-    isolation_unit = models.ForeignKey('UnitsOfTime', models.SET_NULL, blank=True, null=True)
+    isolation_unit = models.ForeignKey("UnitsOfTime", models.SET_NULL, blank=True, null=True)
     with_atb = models.BooleanField(blank=True, null=True)
     other_isolation = models.CharField(max_length=500, null=True, blank=True)
 
@@ -40,3 +43,18 @@ class DiseaseType(models.Model):
     """Model for 'Tipo de enfermedad'"""
     name = models.CharField(max_length=300, null=False, blank=False)
     label = models.CharField(max_length=300, null=True, blank=True)
+
+
+class Disease(models.Model):
+    """Model for 'enfermedad'"""
+    name = models.CharField(max_length=100, null=False, blank=False)
+    label = models.CharField(max_length=100, null=True, blank=True)
+    disease_type = models.ForeignKey(DiseaseType, models.SET_NULL, blank=True, null=True)
+    isolation_type = models.ForeignKey(IsolationType, models.SET_NULL, blank=True, null=True)
+    cleaning_type = models.ManyToManyField(CleaningType, blank=True, null=True, related_name='diseases')
+    isolation_time = models.ManyToManyField(IsolationTime, blank=True, null=True, related_name='diseases')
+    mandatory_declararion = models.BooleanField(null=True, blank=True)
+    isolation_warnings = models.ForeignKey(IsolationWarnings, models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.label
