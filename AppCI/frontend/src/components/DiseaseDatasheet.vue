253604843662660f372994c5e556424147bdcaa3
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-23 22:31:23 -0300

    improve: Add the disease sheet component

---
+++ AppCI/frontend/src/components/DiseaseDatasheet.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="prose-2xl text-center">
+      Dengue
+    </h1>
+    <div class="stats shadow">
+
+      <div class="stat">
+        <div class="stat-title">Total Page Views</div>
+        <div class="stat-value">89,400</div>
+        <div class="stat-desc">21% more than last month</div>
+      </div>
+
+    </div>
+  </div>
+</template>
