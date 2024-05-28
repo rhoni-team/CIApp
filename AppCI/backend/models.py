531c7493473017a0339b8b0ef886a8bffac54daa
nataliamd11 <nataliamdenk11@gmail.com>
2024-05-28 12:05:24 -0300

    fix: change isolation type model by precaution type model

--- AppCI/backend/models.py
+++ AppCI/backend/models.py
@@ -2,8 +2,8 @@
 from django.db import models
 
 
-class IsolationType(models.Model):
-    """Model for 'Tipo de aislamiento'"""
+class PrecautionType(models.Model):
+    """Model for 'Tipo de precaución'"""
     name = models.CharField(max_length=100, null=False, blank=False)
     label = models.CharField(max_length=100, null=True, blank=True)
 
@@ -23,14 +23,10 @@ class UnitsOfTime(models.Model):
     label = models.CharField(max_length=100, null=True, blank=True)
 
 
-class IsolationTime(models.Model):
-    """Model for 'Tiempo de Aislamiento'"""
-    name = models.CharField(max_length=300, blank=True, null=True)
-    label = models.CharField(max_length=300, null=True, blank=True)
-    isolation_time = models.IntegerField(blank=True, null=True)
-    isolation_unit = models.ForeignKey("UnitsOfTime", models.SET_NULL, blank=True, null=True)
-    with_atb = models.BooleanField(blank=True, null=True)
-    other_isolation = models.CharField(max_length=500, null=True, blank=True)
+class SpecialCasesIsolationTime(models.Model):
+    """Special cases of isolation time for some diseases"""
+    disease_name = models.CharField(max_length=100, null=False, blank=False)
+    isolation_time = models.CharField(max_length=300, null=True, blank=True)
 
 
 class IsolationWarnings(models.Model):
@@ -50,9 +46,12 @@ class Disease(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     label = models.CharField(max_length=100, null=True, blank=True)
     disease_type = models.ForeignKey(DiseaseType, models.SET_NULL, blank=True, null=True)
-    isolation_type = models.ForeignKey(IsolationType, models.SET_NULL, blank=True, null=True)
+    precaution_type = models.ForeignKey(PrecautionType, models.SET_NULL, blank=True, null=True)
     cleaning_type = models.ManyToManyField(CleaningType, blank=True, null=True, related_name='diseases')
-    isolation_time = models.ManyToManyField(IsolationTime, blank=True, null=True, related_name='diseases')
+    isolation_time = models.IntegerField(blank=True, null=True)
+    isolation_unit = models.ForeignKey("UnitsOfTime", models.SET_NULL, blank=True, null=True)
+    with_atb = models.BooleanField(blank=True, null=True)
+    other_isolation = models.CharField(max_length=500, null=True, blank=True)
     mandatory_declararion = models.BooleanField(null=True, blank=True)
     room_sharing = models.BooleanField(null=True, blank=True)
     isolation_warnings = models.ForeignKey(IsolationWarnings, models.SET_NULL, blank=True, null=True)
