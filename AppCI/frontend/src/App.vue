943c18563e39112316866b43c7b630e3f4d11668
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-30 18:15:00 -0300

    chore: Move the bottom navbar component from Homeview to main app.vue

--- AppCI/frontend/src/App.vue
+++ AppCI/frontend/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import 'vite/modulepreload-polyfill';
+import BottomNav from './components/BottomNav.vue';
 </script>
 
 <template>
-  <div class="vueapp">
-    <RouterView />
-  </div>
+  <main>
+    <div class="vueapp">
+      <RouterView />
+    </div>
+  </main>
+  <nav>
+    <bottom-nav />
+  </nav>
 </template>
 
 <style scoped></style>
