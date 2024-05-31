07677b11596ef811c56fc7e45106b4f297447079
JNRivadeneira <jnrivadeneira@gmail.com>
2024-05-31 18:45:50 -0300

    test: Add tests to assert the correctly loading for HomeView

--- AppCI/frontend/src/components/__tests__/HomeView.spec.ts
+++ AppCI/frontend/src/components/__tests__/HomeView.spec.ts
@@ -1,10 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import HomeView from "@/views/HomeView.vue";
+import DiseaseSearchInput from "@/components/DiseaseSearchInput.vue";
+import DiseaseDatasheetComponent from "../DiseaseDatasheetComponent.vue";
 
-describe("HomeView", () => {
-  it("should render greeting to rhonies", () => {
+describe("HomeView render correctly", () => {
+  it("DiseaseSearchInput component loaded", () => {
     const wrapper = mount(HomeView);
-    expect(wrapper.text()).toContain("Precauciones");
+    const diseaseSearchInputComponent = wrapper.findComponent(DiseaseSearchInput); // Find the component
+    expect(diseaseSearchInputComponent.exists()).toBe(true);
+  })
+  it("DiseaseDatasheetComponent loaded", () => {
+    const wrapper = mount(HomeView);
+    const diseaseDatasheetComponent = wrapper.findComponent(DiseaseDatasheetComponent); // Find the component
+    expect(diseaseDatasheetComponent.exists()).toBe(true);
   })
 });
--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -8,9 +8,7 @@ import type DiseaseDatasheet from '../types/DiseaseDatasheet';
 
 // components
 import DiseaseDatasheetComponent from '../components/DiseaseDatasheetComponent.vue';
-// import BottomNav from '../components/BottomNav.vue';
 import DiseaseSearchInput from '../components/DiseaseSearchInput.vue';
-// import DarkLightButton from '../components/DarkLightButton.vue';
 
 import { ref } from 'vue';
 
@@ -20,16 +18,11 @@ const diseaseSelectedObject = ref<DiseaseDatasheet | null>(null);
 
 <template>
   <div>
-    <!-- <dark-light-button /> -->
-
     <disease-search-input
       :array-of-sheets="arrayOfSheets"
       @@disease-selected="diseaseSelectedObject = $event"
     />
-
-    <DiseaseDatasheetComponent :disease-sheet="diseaseSelectedObject" />
-    <!-- TODO - Remove clutter from this page -->
-    <!-- <bottom-nav /> -->
+    <disease-datasheet-component :disease-sheet="diseaseSelectedObject" />
   </div>
 </template>
 
