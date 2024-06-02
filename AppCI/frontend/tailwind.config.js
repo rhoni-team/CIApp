aaf130c3bc55765e7cafaa3c0a5924636c1e0482
JNRivadeneira <jnrivadeneira@gmail.com>
2024-06-01 23:20:49 -0300

    chore: Add necessary dependencies for date picker

--- AppCI/frontend/tailwind.config.js
+++ AppCI/frontend/tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+// import colors from 'tailwindcss/colors';
+// const colors = require("tailwindcss/colors");
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/vue-tailwind-datepicker/**/*.js",
   ],
   theme: {
     extend: {
       backgroundImage: {
         // "texture-pattern": "url('@/assets/img/texture.svg')",
       },
+      // colors: {
+      //   "vtd-primary": colors.sky, // Light mode Datepicker color
+      //   "vtd-secondary": colors.gray, // Dark mode Datepicker color
+      // },
     },
   },
   plugins: [
     require("@tailwindcss/typography"), 
-    require("daisyui")
+    require("@tailwindcss/forms"),
+    require("daisyui"),
   ],
 }
 
