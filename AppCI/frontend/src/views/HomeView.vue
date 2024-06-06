d5663fabf56098f1e3e589b2d769eb8cfc772021
JNRivadeneira <jnrivadeneira@gmail.com>
2024-06-06 01:33:40 -0300

    improve: Change the data use logic to matching the 'Data Provider Pattern'

--- AppCI/frontend/src/views/HomeView.vue
+++ AppCI/frontend/src/views/HomeView.vue
@@ -10,17 +10,23 @@ import type DiseaseDatasheet from '../types/DiseaseDatasheet';
 import DiseaseDatasheetComponent from '../components/DiseaseDatasheetComponent.vue';
 import DiseaseSearchInput from '../components/DiseaseSearchInput.vue';
 
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 
 const diseaseSelectedObject = ref<DiseaseDatasheet | null>(null);
+const diseaseSelected = ref<string | null>(null);
+const diseaseList = arrayOfSheets.map((disease) => disease.name);
+
+watchEffect(async () => {
+  diseaseSelectedObject.value = arrayOfSheets.find((disease) => disease.name === diseaseSelected.value) || null;
+});
 
 </script>
 
 <template>
   <div>
     <disease-search-input
-      :array-of-sheets="arrayOfSheets"
-      @@disease-selected="diseaseSelectedObject = $event"
+      :disease-list="diseaseList"
+      @@disease-selected="diseaseSelected = $event"
     />
     <disease-datasheet-component :disease-sheet="diseaseSelectedObject" />
   </div>
