2fe0bacc323b275ba3d81227ef80152e78892c4d
nataliamd11 <nataliamdenk11@gmail.com>
2024-06-06 10:59:35 -0300

    feat: get detail for a given disease

--- AppCI/frontend/src/apiConnections/diseases.js
+++ AppCI/frontend/src/apiConnections/diseases.js
@@ -5,10 +5,11 @@ axios.defaults.xsrfCookieName = "csrftoken";
 
 
 export async function getDiseasesList() {
-    // get geopoints cases from REST API
+    // get list of diseases's names and labels REST API
     try {
       let response = await axios.get("api/diseases");
       let diseases = await response.data
+      console.log(diseases);
       return diseases
     } catch(err) {
       if (err.response) {
@@ -16,3 +17,27 @@ export async function getDiseasesList() {
       }
     }
   }
+
+export async function getDetailedDisease(disease_id) {
+  // get data for a given disease from REST API
+  let disease_data = null;
+  let response_status = null;
+  let error_message = null;
+
+  try {
+    let response = await axios.get("api/diseases/" + disease_id);
+    disease_data = await response.data
+    response_status = response.status;
+    console.log(disease_data);
+    console.log(response_status);
+    return disease_data
+
+  } catch(err) {
+      if (err.response) {
+        response_status = err.response.status
+        error_message = "No existe una enfermedad para el ID ingresado"
+        console.log(error_message);
+        return error_message
+      }
+    }
+}
