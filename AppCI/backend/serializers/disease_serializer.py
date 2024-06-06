335de98d3c2ac93fb426ad1e6e511351fd54ee51
nataliamd11 <nataliamdenk11@gmail.com>
2024-06-06 09:58:31 -0300

    feat: add complete data disease serializer

--- AppCI/backend/serializers/disease_serializer.py
+++ AppCI/backend/serializers/disease_serializer.py
@@ -3,10 +3,18 @@ from rest_framework import serializers
 from backend.models import Disease
 
 
-class ListDiseasesSerializer(serializers.ModelSerializer):
-    """list of diseases model serializer"""
+class DiseasesNamesSerializer(serializers.ModelSerializer):
+    """diseases names and labels model serializer"""
     
     class Meta:
         model = Disease
         fields = ['id', 'name', 'label']
+
+
+class CompleteDataDiseaseSerializer(serializers.ModelSerializer):
+    """diseases serializer with all their data"""
+
+    class Meta:
+        model = Disease
+        fields = "__all__"
         depth = 1
