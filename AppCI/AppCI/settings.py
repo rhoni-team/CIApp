0633a56dd0d2bac89c8d5d4ed6668b9a7d3dba32
nataliamd11 <nataliamdenk11@gmail.com>
2024-09-12 16:11:13 -0300

    chore: read allowed host from .env files

--- AppCI/AppCI/settings.py
+++ AppCI/AppCI/settings.py
@@ -52,7 +52,7 @@ DJANGO_VITE_DEV_SERVER_PORT = 3000
 DJANGO_VITE_MANIFEST_PATH = BASE_DIR / 'frontend' / \
     'static' / 'dist' / '.vite' / 'manifest.json'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(' ')
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:8000", ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
@@ -226,5 +226,5 @@ REST_FRAMEWORK = {
 
 # ssl  ------ UNCOMMENT IN HTTPS
 # SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_SECONDS = 60 # It will be blocked for one minute. Change it if everything is ok.
+# SECURE_HSTS_SECONDS = 10 # It will be blocked for one minute. Change it if everything is ok.
 # SECURE_HSTS_PRELOAD = True
